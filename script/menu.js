025b47e9918bb24b3043f3a1d05049c9e1753d29
Nahuel Luca <nahuelluca@protonmail.com>
2021-01-19 21:15:57 -0300

    first commit

---
+++ script/menu.js
@@ -0,0 +1,10 @@
+const button = document.getElementById('menu-button')
+const menu = document.getElementById('menu')
+
+button.addEventListener('click', function() {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'grid'
+  } else {
+    menu.style.display = 'none'
+  }
+})
