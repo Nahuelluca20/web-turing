2bbc56963b882519c2dda014c5272d466f9f2c65
Nahuel Luca <nahuelluca@protonmail.com>
2021-01-20 17:43:46 -0300

    reveal

---
+++ script/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.home', {delay: 500});
+ScrollReveal().reveal('.why-icons', {delay: 600});
+ScrollReveal().reveal('.services-title', {delay: 500});
+ScrollReveal().reveal('.services-card', {delay: 600});
+ScrollReveal().reveal('.why-description', {delay: 500});
